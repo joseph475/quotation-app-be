920fdd7d21220ceb0e5232bd1b569af994beeddb
joseph475 <markjoseph475@gmail.com>
2025-06-23 19:18:34 +0800

    importin optimize

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
 const app = require('../server');
 
-// For serverless functions, we need to ensure DB connection on each request
-module.exports = async (req, res) => {
-  try {
-    // The connectDB function in server.js will handle connection reuse
-    return app(req, res);
-  } catch (error) {
-    console.error('API handler error:', error);
-    return res.status(500).json({
-      success: false,
-      message: 'Server initialization error'
-    });
-  }
-};
+module.exports = app;
